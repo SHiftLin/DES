dccd8d288a33b2bbb0b2f7d30187d3cac9754eb5
lsh <lshzy137@gmail.com>
2018-10-22 15:23:06 +0800

    bug fixed

--- DES.cpp
+++ DES.cpp
@@ -8,6 +8,12 @@
 #include "DES.h"
 using namespace std;
 
+void Copy(char *dst, const char *src, int n)
+{
+    for (int i = 0; i < n; i++)
+        dst[i] = src[i];
+}
+
 int BitsSplit(char bytes[], int n, char bits[], int base = BASE)
 {
     for (int i = 0; i < n; i++)
@@ -34,7 +40,7 @@ void cshl(char bits[], int n, int k)
     char Temp[n];
     for (int i = 0; i < n; i++)
         Temp[i] = bits[(i + k) % n];
-    strncpy(bits, Temp, n);
+    Copy(bits, Temp, n);
 }
 
 void Permutate(const char src[], const int n, char dst[], int m, int P[])
@@ -52,14 +58,20 @@ void KeysGeneration(char key[], bool isDecode)
     {
         cshl(C, MKEY_SIZE >> 1, LS[i]);
         cshl(C + (MKEY_SIZE >> 1), MKEY_SIZE >> 1, LS[i]);
-        int p = (isDecode) ? ITER_NUM - i : i;
+        int p = (isDecode) ? ITER_NUM - i - 1 : i;
         Permutate(C, MKEY_SIZE, Keys[p], SKEY_SIZE, IPC2);
     }
 }
 
+void XOR(char dst[], const char a[], const char b[], int n)
+{
+    for (int i = 0; i < n; i++)
+        dst[i] = a[i] ^ b[i];
+}
+
 char *F(char bits[], char Key[])
 {
-    char B[SKEY_BYTE], C[SKEY_BYTE];
+    char B[SKEY_SIZE], C[SKEY_SIZE];
     Permutate(bits, HALF_BLK, C, SKEY_SIZE, PE1);
     XOR(B, C, Key, SKEY_SIZE);
     for (int i = 0, k = 0; i < SKEY_SIZE; i += S_SIZE, k++)
@@ -74,24 +86,27 @@ char *F(char bits[], char Key[])
     return bits;
 }
 
-void XOR(char dst[], const char a[], const char b[], int n)
+void Switch(char bits[], int n)
 {
-    for (int i = 0; i < n; i++)
-        dst[i] = a[i] ^ b[i];
+    int m = n >> 1;
+    for (int i = 0; i < m; i++)
+        swap(bits[i], bits[i + m]);
 }
 
 void Run(char bytes[])
 {
     char B[BLK_SIZE], C[BLK_SIZE];
     int n = BitsSplit(bytes, BLK_BYTE, B);
+    print(B, BLK_SIZE);
     Permutate(B, BLK_SIZE, C, BLK_SIZE, IP1);
     for (int i = 0; i < ITER_NUM; i++)
     {
         char *L = C, *R = C + HALF_BLK;
-        strncpy(B, R, HALF_BLK);
-        XOR(B, L, F(R, Keys[i]), HALF_BLK);
-        strncpy(C, B, BLK_SIZE);
+        Copy(B, R, HALF_BLK);
+        XOR(B + HALF_BLK, L, F(R, Keys[i]), HALF_BLK);
+        Copy(C, B, BLK_SIZE);
     }
+    Switch(C, BLK_SIZE);
     Permutate(C, BLK_SIZE, B, BLK_SIZE, IP2);
     int m = BitsMerge(B, BLK_SIZE, bytes);
 }
@@ -99,18 +114,22 @@ void Run(char bytes[])
 int main(int argc, char **argv)
 {
     char key[KEY_BYTE + 5] = "abcdefgh";
-    bool isDecode = false;
+    bool isDecode = true;
     KeysGeneration(key, isDecode);
 
-    FILE *fin = fopen("test.txt", "r");
-    FILE *fout = fopen("test.des", "w");
+    FILE *fin = fopen("TEST/test.txt", "r");
+    FILE *fout = fopen("TEST/test.des", "w");
 
     char block[BLK_BYTE];
     for (int cnt = 0; (cnt = fread(block, 1, BLK_BYTE, fin)) != 0;)
     {
         for (int i = cnt; i < BLK_BYTE; i++)
             block[i] = 0;
+        printf("%s\n", block);
         Run(block);
+        for (int i = 0; i < BLK_BYTE; i++)
+            printf("%x ", block[i]);
+        printf("\n");
         fwrite(block, 1, BLK_BYTE, fout); //decode????
     }
     fclose(fin);
