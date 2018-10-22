3262e6c039aa82f1a4ecbf22b57f510be3e754fa
lsh <lshzy137@gmail.com>
2018-10-22 15:30:26 +0800

    reorganized

--- DES.cpp
+++ DES.cpp
@@ -6,48 +6,8 @@
 #include <cmath>
 #include <algorithm>
 #include "DES.h"
+#include "Util.h"
 using namespace std;
-
-void Copy(char *dst, const char *src, int n)
-{
-    for (int i = 0; i < n; i++)
-        dst[i] = src[i];
-}
-
-int BitsSplit(char bytes[], int n, char bits[], int base = BASE)
-{
-    for (int i = 0; i < n; i++)
-        for (int j = base - 1, x = bytes[i]; j >= 0; j--, x >>= 1)
-            bits[i * base + j] = x & 1;
-    return n * base;
-}
-
-int BitsMerge(char bits[], int n, char bytes[])
-{
-    int cnt = 0;
-    for (int i = 0; i < n; i += BASE)
-    {
-        int x = 0;
-        for (int j = 0; j < BASE; j++)
-            x = (x << 1) | bits[i + j];
-        bytes[cnt++] = x;
-    }
-    return cnt;
-}
-
-void cshl(char bits[], int n, int k)
-{
-    char Temp[n];
-    for (int i = 0; i < n; i++)
-        Temp[i] = bits[(i + k) % n];
-    Copy(bits, Temp, n);
-}
-
-void Permutate(const char src[], const int n, char dst[], int m, int P[])
-{
-    for (int i = 0; i < m; i++)
-        dst[i] = src[P[i]];
-}
 
 void KeysGeneration(char key[], bool isDecode)
 {
@@ -61,12 +21,6 @@ void KeysGeneration(char key[], bool isDecode)
         int p = (isDecode) ? ITER_NUM - i - 1 : i;
         Permutate(C, MKEY_SIZE, Keys[p], SKEY_SIZE, IPC2);
     }
-}
-
-void XOR(char dst[], const char a[], const char b[], int n)
-{
-    for (int i = 0; i < n; i++)
-        dst[i] = a[i] ^ b[i];
 }
 
 char *F(char bits[], char Key[])
@@ -86,14 +40,7 @@ char *F(char bits[], char Key[])
     return bits;
 }
 
-void Switch(char bits[], int n)
-{
-    int m = n >> 1;
-    for (int i = 0; i < m; i++)
-        swap(bits[i], bits[i + m]);
-}
-
-void Run(char bytes[])
+void DES(char bytes[])
 {
     char B[BLK_SIZE], C[BLK_SIZE];
     int n = BitsSplit(bytes, BLK_BYTE, B);
@@ -126,7 +73,7 @@ int main(int argc, char **argv)
         for (int i = cnt; i < BLK_BYTE; i++)
             block[i] = 0;
         printf("%s\n", block);
-        Run(block);
+        DES(block);
         for (int i = 0; i < BLK_BYTE; i++)
             printf("%x ", block[i]);
         printf("\n");
---
+++ Util.h
@@ -0,0 +1,62 @@
+#include <iostream>
+
+void print(const char v[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d", v[i]);
+    printf("\n");
+}
+
+void Copy(char *dst, const char *src, int n)
+{
+    for (int i = 0; i < n; i++)
+        dst[i] = src[i];
+}
+
+int BitsSplit(char bytes[], int n, char bits[], int base = 8)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = base - 1, x = bytes[i]; j >= 0; j--, x >>= 1)
+            bits[i * base + j] = x & 1;
+    return n * base;
+}
+
+int BitsMerge(char bits[], int n, char bytes[], int base = 8)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i += base)
+    {
+        int x = 0;
+        for (int j = 0; j < base; j++)
+            x = (x << 1) | bits[i + j];
+        bytes[cnt++] = x;
+    }
+    return cnt;
+}
+
+void Permutate(const char src[], const int n, char dst[], int m, int P[])
+{
+    for (int i = 0; i < m; i++)
+        dst[i] = src[P[i]];
+}
+
+void XOR(char dst[], const char a[], const char b[], int n)
+{
+    for (int i = 0; i < n; i++)
+        dst[i] = a[i] ^ b[i];
+}
+
+void cshl(char bits[], int n, int k)
+{
+    char Temp[n];
+    for (int i = 0; i < n; i++)
+        Temp[i] = bits[(i + k) % n];
+    Copy(bits, Temp, n);
+}
+
+void Switch(char bits[], int n)
+{
+    int m = n >> 1;
+    for (int i = 0; i < m; i++)
+        swap(bits[i], bits[i + m]);
+}
